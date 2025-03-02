cf893471d15e003799b3ec7d0756b49efebef95f
ntondama <ntondama31@gmail.com>
2025-03-02 20:27:10 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-12345678"
+}
+
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
